33d3ca42e52710d097812c8a91f553a45ad2ddfb
Adi Narayan <adinarayanrs777@gmail.com>
2025-05-31 02:20:23 +0400

    Categories scroll view done

---
+++ app/src/main/java/com/example/coffeeapp/Activity/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.coffeeapp.Activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.coffeeapp.Adapter.CategoryAdapter
+import com.example.coffeeapp.ViewModel.MainViewModel
+import com.example.coffeeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBanner()
+        initcategory()
+    }
+
+    private fun initBanner() {
+        binding.progressBarBanner.visibility = View.VISIBLE
+        viewModel.loadBanner().observeForever()
+        {
+            Glide.with(this@MainActivity)
+                .load(it[0].url)
+                .into(binding.banner)
+            binding.progressBarBanner.visibility = View.GONE
+        }
+        viewModel.loadBanner()
+    }
+
+private fun initcategory(){
+        binding.progressBarCategory.visibility = View.VISIBLE
+        viewModel.loadCategory().observeForever {
+            binding.recyclerViewCat.layoutManager =
+                LinearLayoutManager(
+                    this@MainActivity, LinearLayoutManager.HORIZONTAL,
+                    false
+                )
+
+            binding.recyclerViewCat.adapter= CategoryAdapter(it)
+            binding.progressBarCategory.visibility=View.GONE
+        }
+        viewModel.loadCategory()
+    }
+}
